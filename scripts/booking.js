ab20a1bf62de78652bc512cb6a02a338b33d3f8c
mjm29 <MarcJoseph.Millare@edu.sait.ca>
2026-04-10 10:53:02 -0600

    added functionality to half day full day buttons, cost is now recalculated for every button pressed

--- scripts/booking.js
+++ scripts/booking.js
@@ -3,10 +3,12 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 let numDaysSelected = 0;
+let dailyRate = 35;
 const days = document.querySelectorAll("li")
 const totalCost = document.getElementById("calculated-cost");
 const clearDays = document.getElementById("clear-button");
-
+const halfDay = document.getElementById("half");
+const fullDay = document.getElementById("full");
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
@@ -16,6 +18,7 @@ function clickDay(){
     numDaysSelected += 1;
     this.classList.add("clicked");
     console.log(numDaysSelected);
+    recalculate();
   }
 }
 days.forEach(day => {
@@ -42,17 +45,30 @@ clearDays.addEventListener("click", reset);
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
-
-
+function setPriceHalf(){
+  dailyRate = 20;
+  halfDay.classList.add("clicked");
+  fullDay.classList.remove("clicked");
+  recalculate();
+}
+halfDay.addEventListener("click", setPriceHalf);
 
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
-
+function setPriceFull(){
+  dailyRate = 35;
+  fullDay.classList.add("clicked");
+  halfDay.classList.remove("clicked");
+  recalculate();
+}
+fullDay.addEventListener("click", setPriceFull);
 
 
 
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
-
+function recalculate(){
+  totalCost.innerHTML = dailyRate*numDaysSelected;
+}
 
